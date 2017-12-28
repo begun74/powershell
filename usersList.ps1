97c1994f659f4d68bb7e422efc0ada3057cbe77d
Бегун Владимир Васильевич <D:\Mail>
2017-12-28 13:16:45 +0300

    git commit

---
+++ usersList.ps1
@@ -0,0 +1,51 @@
+﻿Import-Module ActiveDirectory
+
+
+function get-adusers() {
+	
+	Param(
+		[Parameter(Mandatory=$false)] [String]$isEnabled="True"
+	)
+
+	Write-Output "isEnabled - " $isEnabled
+
+	###### $listAdusers = Get-ADUser -Filter {Name -like "qwe*"} -SearchBase "OU=�� ������������,DC=interfarmax,DC=local" -Properties * | select Name,SID,PostalCode | Export-Csv '.\ifx_local.csv'
+
+	$listAdusers = (Get-ADUser -Filter {Name -like "qwe*" -and Enabled -eq $isEnabled} -SearchBase "OU=ГК ИНТЕРФАРМАКС,DC=interfarmax,DC=local" -Properties *)
+
+	return $listAdusers
+}
+
+
+function set-adusers() {
+
+	$listAdusers = get-adusers
+
+
+	If($listAdusers.count -ne 0 ) {
+
+		ForEach ($user in $listAdusers) { 
+			if($user.GetType().Name -eq "ADUser") { #Проверяем тип обьекта
+
+			
+				Write-Output $user.Name
+			
+				$Attributes = @{
+					"postalCode" = $user.postOfficeBox[0]
+					#"postalCode" = $user.st
+				}
+			
+				Set-AdUser -Identity $user.SamAccountName @Attributes
+			}
+		}
+
+	}
+} #-- function
+
+function main() {
+
+	set-adusers
+}
+
+main
+
