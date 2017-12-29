63d172c3579b186e3cbbcb3a9cc18d0d43ee6fdf
Бегун Владимир Васильевич <D:\Mail>
2017-12-29 10:22:03 +0300

    asdf

--- usersList.ps1
+++ usersList.ps1
@@ -12,7 +12,7 @@ function get-adusers() {
 
 	###### $listAdusers = Get-ADUser -Filter {Name -like "qwe*"} -SearchBase "OU=ГК ИНТЕРФАРМАКС,DC=interfarmax,DC=local" -Properties * | select Name,SID,PostalCode | Export-Csv '.\ifx_local.csv'
 
-	$listAdusers = (Get-ADUser -Filter {Name -like "qwe*" -and Enabled -eq $isEnabled} -SearchBase "OU=ГК ИНТЕРФАРМАКС,DC=interfarmax,DC=local" -Properties *)
+	$listAdusers = (Get-ADUser -Filter {SamAccountName -like "qwe*" -and Enabled -eq $isEnabled} -SearchBase "OU=ГК ИНТЕРФАРМАКС,DC=interfarmax,DC=local" -Properties *)
 
 	return $listAdusers
 }
@@ -48,16 +48,21 @@ function set-adusers() {
 
 function move-adusers () {
 	Param(
-		[Parameter(Mandatory=$false)] [Object[]]$listAdusers,
-		
-		[Parameter(Mandatory=$false)] [boolean]$isEnable=0
+		[Parameter(Mandatory=$false,Position=0)] [Object[]]$listAdusers,
+
+		[Parameter(Mandatory=$false,Position=1)] [boolean]$isEnable=0,
+
+		[Parameter(Mandatory=$false,Position=2)] [string]$Target_Path = "OU=Уволенные,OU=ГК ИНТЕРФАРМАКС,DC=interfarmax,DC=local"
+
 	)
+
+	Write-Host   "Target_Path - "  $Target_Path 
 
 	ForEach ($user in $listAdusers) { 
 		if($user.GetType().Name -eq "ADUser" -and $user.Enabled -eq $isEnable) { #Проверяем тип обьекта и чтоб был отключён
 		
-			Write-Output  $user.Name " - " $user.Enabled
-		
+			Write-Host   "Target_Path - "  $Target_Path 
+			Move-ADObject -Identity $user -TargetPath $Target_Path
 		}
 	}
 
@@ -65,8 +70,9 @@ function move-adusers () {
 
 function main() {
 	$listAdusers = get-adusers("False")     #Array of ADUsers
+	#$listAdusers = get-adusers     #Array of ADUsers
 
-	set-adusers($listAdusers)
+	#set-adusers($listAdusers)
 
 	move-adusers($listAdusers)
 }
